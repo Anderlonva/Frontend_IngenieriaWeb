89916c0198f3e2b0f520b1045345bbbcaa3ded9a
Anderson Londoño <84043589+Anderlonva@users.noreply.github.com>
2022-09-28 22:13:55 -0500

    modulos usuarios,marca,etc

---
+++ src/components/usuarios/UsuarioRowTable.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+export const UsuarioRowTable = ({ usuario, usuarios }) => {
+
+    
+    return (
+        <tr>
+            
+            <td>{usuario.nombre}</td>
+            <td>{usuario.email}</td>
+            <td>{usuario.estado}</td>
+            <td>{usuario.fechaCreacion}</td>
+            <td>{usuario.fechaActualizacion}</td>
+            
+            <td><Link type="button" className="btn btn-success" to={`usuario/edit/${usuario._id}`}>Editar</Link></td>
+        </tr>
+    )
+}
